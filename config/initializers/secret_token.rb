e53db72875cf3625b353a2a2c5fd656a65232911
jimmywales <jimmywales@ymail.com>
2012-08-15 15:20:21 +0100

    First commit of actual project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlowingCyril::Application.config.secret_token = '2b163d1eebc1a14c8c467d30c933b14b50f6f9948bfb27ff672128b0442d8ef1243bb9de2b0514204a9da836c1cd1429c661f2cafe44d9d8ae239cff836c359d'
